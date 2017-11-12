3008f806bc94d2e6449680563d450e0979626ffe
takirchjunger <tom.kirchjunger@topicus.education>
2017-11-12 21:44:22 +0100

    Trying to make ID work

--- addon/models/cart-item.js
+++ addon/models/cart-item.js
@@ -1,32 +1,33 @@
 import Ember from 'ember';
 
 const {
-  get,
-  computed,
-  guidFor,
-  Object: EmberObject
+    get,
+    computed,
+    guidFor,
+    Object: EmberObject
 } = Ember;
 
 export default EmberObject.extend({
-  increment: true,
-  quantity: 0,
-  price: 0,
+    id: -1,
+    increment: true,
+    quantity: 0,
+    price: 0,
 
-  total: computed('quantity', 'price', function() {
-    return get(this, 'quantity') * get(this, 'price');
-  }),
+    total: computed('quantity', 'price', function () {
+        return get(this, 'quantity') * get(this, 'price');
+    }),
 
-  guidProps: ['name'],
+    guidProps: ['name'],
 
-  guid: computed('guidProps', function() {
-    let guidVal = get(this, 'guidProps').sort().map((prop) => {
-      return get(this, prop);
-    }).join('-');
+    guid: computed('guidProps', function () {
+        let guidVal = get(this, 'guidProps').sort().map((prop) => {
+            return get(this, prop);
+        }).join('-');
 
-    return guidFor(guidVal);
-  }),
+        return guidFor(guidVal);
+    }),
 
-  toCartItem() {
-    return this;
-  }
+    toCartItem() {
+        return this;
+    }
 });
